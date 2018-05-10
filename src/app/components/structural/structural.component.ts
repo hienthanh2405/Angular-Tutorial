c8f2c09ab440633d8d86f959702f1f4a831e19c2
My-PC\My <hienthanh2405@gmail.com>
2018-05-10 20:40:02 +0700

    ngFor

--- src/app/components/structural/structural.component.ts
+++ src/app/components/structural/structural.component.ts
@@ -7,22 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuralComponent implements OnInit {
 
-  public isShow : boolean = false;
-  public userName : string = "Nguyen van A";
-  public isChecked : boolean = false;
-  public age : number;
+  public isShow: boolean = false;
+  public userName: string = "Nguyen van A";
+  public isChecked: boolean = false;
+  public age: number;
+  public names: string[] = ['TPHCM', 'Hà Nội', 'Vĩnh Long', 'Cần thơ'];
+  public dataFromServer : string[] = ['TPHCM', 'Hà Nội', 'Vĩnh Long', 'Cần thơ', 'Hạ Long', 'Tiền Giang'];
+  public products: any[] = [
+    {
+      id: 1,
+      name: 'Iphone 6 plus',
+      price: 200000
+    },
+    {
+      id: 2,
+      name: 'Iphone 7 plus',
+      price: 200000
+    },
+    {
+      id: 3,
+      name: 'samsung S6',
+      price: 200000
+    },
+    {
+      id: 4,
+      name: 'samsung S7',
+      price: 200000
+    },
+    {
+      id: 5,
+      name: 'samsung S8',
+      price: 200000
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-  
-  onToggle(){
+
+  onToggle() {
     this.isShow = !this.isShow;
   }
 
-  onChange(value){
+  onChange(value) {
     this.isChecked = value;
   }
+
+  onLoadData(){
+    this.names = this.dataFromServer;
+  }
+  myTrackByFunction(index, item){
+    return item.id;
+  }
+
 
 }
